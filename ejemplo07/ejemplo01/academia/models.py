67621ee1efd958c5df8661e6fca85bdd46d0fabc
LuisAg1256 <laaguilar21@utpl.edu.ec>
2026-06-11 12:06:48 -0500

    Models Actualizado

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -8,6 +9,16 @@ class Estudiante(models.Model):
     cedula = models.CharField(max_length=30, unique=True)
     edad = models.IntegerField()
 
+    def anio_nacimiento(self):
+        anio_actual = datetime.now().year
+        valor = anio_actual - self.edad
+        return valor
+    
+    def obtener_ciudad(self):
+        if(self.cedula.startswith("11")):
+            return "Loja"
+        else:
+            return "Otra Ciudad"
 
     def __str__(self):
-        return f"nombre: {self.nombre} - apellido: {self.apellido} - cedula: {self.cedula} - edad: {self.edad}"
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - CI: {self.obtener_ciudad()} - Edad: {self.edad} - Año de Nacimiento: {self.anio_nacimiento()}"
